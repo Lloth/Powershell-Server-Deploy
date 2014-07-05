c00cf99fd31525a6578b9e8cba4fc17ff09ec6fb
Lloth <llothsedai@gmail.com>
2014-07-05 17:32:37 -0400

    Delete DeployTeamviewer.ps1

--- DeployTeamviewer.ps1
+++
@@ -1,9 +0,0 @@
-mkdir D:\itradix\teamviewer
-mv \\drobo-fs\ITR TOOLS\ServerDeployment\TeamViewerMSI_itradix.zip D:\itradix\Teamviewer
-$shell = new-object -com shell.application
-$zip = $shell.NameSpace("D:\itradix\Teamviewer\TeamViewerMSI_itradix.zip")
-foreach($item in $zip.items())
-{
-$shell.Namespace("D:\itradix\Teamviewer\").copyhere($item)
-}
-msiexec /i D:\itradix\Teamviewer\Teamviewer_host.msi /quiet
