d60b14a35371969919515d9f66efb4d4dfb779ba
Lloth <llothsedai@gmail.com>
2014-07-14 20:23:36 -0400

    Install x64 version of openmange
    Corrected path info
    - Still needs 64 vs 32bit.
    - Also need to test whether or not "all" is the right thing to install.

--- InstallOpenmanage.ps1
+++ InstallOpenmanage.ps1
@@ -3,6 +3,7 @@ function GetFileAndVerify ($f_filepath, $f_filename, $f_filecachelocation, $f_fi
 
     $f_pathOfFile =  $f_filepath + "\" + $f_filename
     $f_pathOfCache =  $f_filecachelocation + "\" + $f_filename
+   
 
         New-Item $f_filepath -type directory -ErrorAction SilentlyContinue
     
@@ -11,11 +12,15 @@ function GetFileAndVerify ($f_filepath, $f_filename, $f_filecachelocation, $f_fi
             write-host "File Already Exists"
             return $true
         } else {
-            if(Test-Path $f_filecachelocation -ErrorAction SilentlyContinue)  #Download from Local Share
+            if(Test-Path $f_filecachelocation)  #Download from Local Share
             {
+             
                 write-host "Get file from local share"
+               
                 copy-item $f_pathOfCache -destination $f_pathOfFile 
+                 write-host $f_pathOfCache
             }else {
+                
                 write-host "Download the file from web" 
                 wget $f_webDownload -OutFile $f_pathOfFile   
             }
@@ -33,15 +38,24 @@ function GetFileAndVerify ($f_filepath, $f_filename, $f_filecachelocation, $f_fi
 }
 $filepath = "C:\tools\openmanage"
 $filename = "OM_SMTD_740_A00.iso"
-$filecachelocation = "\\locationoffile\share\"
+$filecachelocation = ""
 $fileHash = "f2ec721d988585faa72191fdaf2df7236a9cf9f3c3956a08eae422eb2e4b3932"
 $webDownload  = "ftp://ftp.dell.com/secure//FOLDER02027063M/1/OM_SMTD_740_A00.iso"  
 
 $pathOfFile =  $filepath + "\" + $filename
 $pathOfCache =  $filecachelocation + "\" + $filename 
 
+
+
 if(GetFileAndVerify $filepath $filename $filecachelocation $fileHash $webDownload)
-{
-Mount-DiskImage -ImagePath ($pathOfFile)
-msiexec.exe /i SysMgmt.msi /qn
+{$beforeMount = (Get-Volume).DriveLetter
+
+$mountResult = Mount-DiskImage -ImagePath ($pathOfFile)
+
+$setuppath = (compare $beforeMount (Get-Volume).DriveLetter -PassThru) + ":\"
+$setuppath
+cd  $setuppath
+cd ".\SYSMGMT\srvadmin\windows\SystemsManagementx64\"
+msiexec.exe /i SysMgmtx64.msi ADDLOCAL="ALL" /qn
+Dismount-DiskImage $setuppath
 }
